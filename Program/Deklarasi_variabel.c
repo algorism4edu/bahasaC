c7938f40aad0cb1e1b7f2b83e70d0bc960e45f8d
algorism4edu <47321047+algorism4edu@users.noreply.github.com>
2019-04-24 21:04:25 +0800

    Create Deklarasi_variabel.c

---
+++ Program/Deklarasi_variabel.c
@@ -0,0 +1,36 @@
+// Program - Definisi variabel dalam bahasa C
+#include <stdio.h>
+
+int main(void)
+{
+  printf("uang saya sebesar 10000 rupiah");
+  return 0;
+}
+/*  Selesai */
+
+/* Penggunaan variabel */
+//int main(void)
+//{
+//  int uang;     // deklarasi variabel bertipe integer (bilangan bulat)
+//  uang = 10000; // menyimpan 10000 ke variabel uang
+//  printf("uang saya sebesar %d rupiah\n", uang);
+//  // %d untuk tipe variabel integer
+//  // printf (control string, nama variabel);
+//  return 0;
+//}
+/*  Selesai */
+
+/* penggunaan banyak variabel */
+//int main(void)
+//{
+//  int roti; // deklarasi variabel roti
+//  int harga; // deklarasi variabel harga
+//
+//  roti = 4; // simpan 4 di variabel roti
+//  harga = 5000; // simpan 5000 di variabel harga
+//
+//  // tampilkan hasil
+//  printf("%d roti seharga %d rupiah\n", roti, harga);
+//  return 0;
+//}
+/*  Selesai */
